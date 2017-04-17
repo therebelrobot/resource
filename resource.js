88f84dc5bf7e25d82d34d0f495a8d4d376096ec5
Trent Oswald <trentoswald@therebelrobot.com>
2017-04-17 16:19:04 -0700

    🎉  Initial push

---
+++ resource.js
@@ -0,0 +1,83 @@
+function initResource (config) {
+  const { resources, connectors } = config
+  async function generateResource (type, method, options, viewer) {
+    log(`Generating ${type} ${method} resource`)
+    const resource = new Resource(type, method, viewer)
+    log(`Validating ${type} ${method} request`)
+    await resource.validate(options)
+    log(`Executing ${type} ${method} request`)
+    await resource.exec(options)
+    log(`Sanitizing and returning ${type} ${method} request`)
+    return resource.sanitize()
+  }
+  class Resource {
+    constructor (type, method, viewer) {
+      this.resources = resources
+      this.context = { viewer, connectors }
+
+      this.methodExists = methodExists
+      this.typeExists = typeExists
+
+      this.type = type
+      this.typeExists()
+
+      this.method = method
+      this.methodExists()
+    }
+    validate (...args) { return validate.apply(this, args) }
+    exec (...args) { return exec.apply(this, args) }
+    sanitize (...args) { return sanitize.apply(this, args) }
+  }
+  return {
+    Resource,
+    generateResource
+  }
+}
+
+function typeExists () {
+  if (!Object.keys(this.resources).includes(this.type)) throw new Error(`${this.type} does not exist as a resource.`)
+}
+
+function methodExists () {
+  if (!this.resources[this.type][this.method] || typeof this.resources[this.type][this.method] !== 'function') throw new Error(`${this.type} does not contain a ${this.method} method`)
+}
+
+function log(...args) {
+  if (process.env.VERBOSE) console.log.apply(console, args)
+}
+
+async function validate (options) {
+  this.valid = await this.resources[this.type].validate(this.method, options, this.context)
+  return this.valid
+}
+
+async function exec (options) {
+  if (this.type.constructor === Array) { // allow for exec chaining
+    for (let typeIndex in this.type) {
+      options.previous_results = options.previous_results || {}
+      options.previous_results[this.type[typeIndex]] = this.results // store previous results for inspection further in the chain
+      this.results = await this.resources[this.type[typeIndex]][this.method](options, this.context) // update results
+    }
+  } else { // else run just one exec
+    this.results = await this.resources[this.type][this.method](options, this.context)
+  }
+  return this.results
+}
+
+async function sanitize () {
+  this.methodExists('sanitize')
+  this.sanitizeResults = await this.resources[this.type].sanitize(this.method, this.results, this.context)
+  return this.sanitizeResults
+}
+
+module.exports = {
+  initResource,
+  __internals: {
+    typeExists,
+    methodExists,
+    log,
+    validate,
+    exec,
+    sanitize
+  }
+}
